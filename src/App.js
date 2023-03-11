44fe4f2e67abb87e5a9ec27c42809d2b042abf6e
sala-design <solahudeenbabatunde@gmail.com>
2023-03-11 11:17:08 +0100

    Added a loading gif

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import axios from "axios";
 import NFTModal from "./components/NFTModal";
 import NFTCard from "./components/NFTCard";
 import styled from "styled-components";
+import loadingGif from "./assets/loader.gif";
 
 const App = () => {
   // Use a constant variable for the default address
   const defaultAddress = "0x8767B976Ba24284374933334fc117248554F5745";
-  
+
   // Use a state variable to store the search query
   const [query, setQuery] = useState(defaultAddress);
 
@@ -17,9 +18,12 @@ const App = () => {
   // Use a state variable to store the selected NFT object
   const [selectedNFT, setSelectedNFT] = useState(null);
 
+  const [isLoading, setIsLoading] = useState(false);
+
   // Define event handlers for the search input field and NFT card clicks
   const handleSearch = () => {
     async function fetchNFTs() {
+      setIsLoading(true);
       try {
         const response = await axios.get(
           `https://api.opensea.io/api/v1/assets?owner=${query}`
@@ -27,6 +31,8 @@ const App = () => {
         setNFTs(response.data.assets);
       } catch (error) {
         console.error(error);
+      } finally {
+        setIsLoading(false); // Stop the loading animation when the API request finishes
       }
     }
     fetchNFTs();
@@ -46,6 +52,7 @@ const App = () => {
 
   useEffect(() => {
     async function fetchNFTs() {
+      setIsLoading(true);
       try {
         const response = await axios.get(
           `https://api.opensea.io/api/v1/assets?owner=${defaultAddress}`
@@ -53,6 +60,9 @@ const App = () => {
         setNFTs(response.data.assets);
       } catch (error) {
         console.error(error);
+      } finally {
+        // Stop the loading animation when the API request finishes
+        setIsLoading(false);
       }
     }
     fetchNFTs();
@@ -60,62 +70,77 @@ const App = () => {
 
   return (
     <div>
-      <h3 style={{textAlign:"center"}}>Enter your address below</h3>
-      <SearchContainer >
-        <input type="text" onChange={handleInputChange} value={query} placeholder="Search..." />
-        <SearchButton type="button" onClick={handleSearch}>search</SearchButton>
-      </SearchContainer >
-      <NFTGrid >
-        {/* Use optional chaining to check if nfts is not null or undefined before mapping over it */}
-        {nfts?.map((nft) => (
-          <NFTCard key={nft.token_id} nft={nft} onClick={handleCardClick} />
-        ))}
-        {/* Use a conditional rendering to display the NFTModal only if a NFT object is selected */}
-        {selectedNFT && (
-          <NFTModal nft={selectedNFT} onClose={handleModalClose} />
-        )}
-      </NFTGrid>
+      <h3 style={{ textAlign: "center" }}>Enter your address below</h3>
+      <SearchContainer>
+        <input
+          type="text"
+          onChange={handleInputChange}
+          value={query}
+          placeholder="Search..."
+        />
+        <SearchButton type="button" onClick={handleSearch}>
+          search
+        </SearchButton>
+      </SearchContainer>
+      {isLoading ? ( // Step 3: Use conditional rendering to display the loading GIF if the API request is in progress
+        <Loader>
+        <img src={loadingGif} alt="Loading..." />
+        </Loader>
+      ) : (
+        <NFTGrid>
+          {nfts?.map((nft) => (
+            <NFTCard key={nft.token_id} nft={nft} onClick={handleCardClick} />
+          ))}
+          {selectedNFT && (
+            <NFTModal nft={selectedNFT} onClose={handleModalClose} />
+          )}
+        </NFTGrid>
+      )}
     </div>
   );
 };
-
- const NFTGrid =styled.div`
- display: grid;
- grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
- gap: 1rem;
- margin: 2rem auto;
- max-width: 1200px;
-
- `
- const SearchContainer =styled.div`
- display: flex;
- align-items: center;
- width: 100%;
- max-width: 400px;
- background-color: #f2f2f2;
- border-radius: 5px;
- overflow: hidden;
- margin:auto;
- input{
-  flex: 1;
+const Loader = styled.div`{
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}`
+const NFTGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  gap: 1rem;
+  margin: 2rem auto;
+  max-width: 1200px;
+`;
+const SearchContainer = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  max-width: 400px;
+  background-color: #f2f2f2;
+  border-radius: 5px;
+  overflow: hidden;
+  margin: auto;
+  input {
+    flex: 1;
+    height: 40px;
+    padding: 10px;
+    border: none;
+    font-size: 16px;
+    background-color: transparent;
+    outline: none;
+  }
+`;
+const SearchButton = styled.button`
   height: 40px;
-  padding: 10px;
+  width: 60px;
+  background-color: #4caf50;
+  color: #fff;
   border: none;
   font-size: 16px;
-  background-color: transparent;
-  outline:none;
- }
- `
- const SearchButton=styled.button`
- height: 40px;
- width: 60px;
- background-color: #4CAF50;
- color: #fff;
- border: none;
- font-size: 16px;
- cursor: pointer;
- &:hover{
-  background-color: #45a049;
- }
- `
+  cursor: pointer;
+  &:hover {
+    background-color: #45a049;
+  }
+`;
 export default App;
